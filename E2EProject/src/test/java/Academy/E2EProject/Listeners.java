68e200c1b9c21a4493a6b4eb2026a3893b014729
Alex <alextshiswaka@gmail.com>
2020-10-10 19:05:24 -0400

    listeners cleanup

--- E2EProject/src/test/java/Academy/E2EProject/Listeners.java
+++ E2EProject/src/test/java/Academy/E2EProject/Listeners.java
@@ -54,29 +54,4 @@ public class Listeners extends Base implements ITestListener{
 		}
 	}
 
-	public void onTestSkipped(ITestResult result) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onTestFailedWithTimeout(ITestResult result) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onStart(ITestContext context) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	public void onFinish(ITestContext context) {
-		// TODO Auto-generated method stub
-		extent.flush();
-	}
-
 }
